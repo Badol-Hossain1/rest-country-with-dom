9922d717fffbd79436b28b2067527128b07f33ee
Badol-Hossain1 <badolhossain586@gmail.com>
2023-04-29 00:48:01 +0600

    fect: added the dynamic

--- random.js
+++ random.js
@@ -22,24 +22,30 @@ const displayCountrys = (datas) => {
   const country = document.getElementById("country");
 
   datas.forEach((element) => {
-    console.log(
-      "🚀 ~ file: random.js:25 ~ displayCountrys ~ element:",
-      element,
-    );
     const li = document.createElement("div");
     li.classList.add("contrys");
     li.innerHTML = `
     <h3> name: ${element.continents} </h3>
-    <p> ${element.capital ? element.capital[0]:"no capital"} </p>
+    <p> ${element.capital ? element.capital[0] : "no capital"} </p>
     <button onclick="showDetails('${element.cca2}')">details </button>
     `;
     country.appendChild(li);
   });
 };
 
-const showDetails = code => {
-    // https://restcountries.com/v3.1/alpha/{code}
-    const details = `https://restcountries.com/v3.1/alpha/${code}`
-    console.log('data is comming ',code);
-}
+const showDetails = (code) => {
+  // https://restcountries.com/v3.1/alpha/{code}
+  const details = `https://restcountries.com/v3.1/alpha/${code}`;
+  fetch(details)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data));
+};
+
+const displayDetails = (e) => {
+  console.log(e[0].name.common);
+  const display = document.getElementById('display')
+  display.innerHTML = `
+  <h1> country name : ${e[0].name.common} </h1>
+  `
+};
 countrys();
